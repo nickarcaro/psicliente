fe52608334c21ab2583c27245683cd6f62919a03
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-12-02 20:09:06 -0300

    add chart bar

--- src/components/Admin/Profile/MyProfile.js
+++ src/components/Admin/Profile/MyProfile.js
@@ -1,5 +1,28 @@
-import React from "react";
+adimport React, { useState, useEffect } from "react";
+import { List, Button } from "antd";
+import { Bar } from "react-chartjs-2";
 
-export default function Profile() {
-  return <div></div>;
+export default function Profile(props) {
+  const { consultants, setReloadConsultants } = props;
+  //console.log(consultants);
+  let edades = [];
+  let motivos = [];
+  for (let dataObj of consultants) {
+    console.log(dataObj);
+    edades.push(dataObj.edad);
+    motivos.push(dataObj.motivo);
+  }
+
+  let data = {
+    labels: motivos,
+    datasets: [
+      {
+        label: "Porcentaje de trastornos mentales",
+        data: edades,
+        backgroundColor: ["green", "red", "rgb(255, 153, 51)", "blue"],
+      },
+    ],
+  };
+
+  return <Bar data={data} />;
 }
--- src/pages/Admin/Profile/Profile.js
+++ src/pages/Admin/Profile/Profile.js
@@ -1,12 +1,27 @@
-import React from "react";
-//import { getAccessTokenApi } from "../../../api/auth";
-//import { getUsers } from "../../../api/user";
-//import MyProfile from "../../../components/Admin/Profile";
+import React, { useState, useEffect } from "react";
+import { getAccessTokenApi } from "../../../api/auth";
+import { getConsultants } from "../../../api/consultantes";
+import MyProfile from "../../../components/Admin/Profile";
 
 export default function Profile() {
+  const [consultants, setConsultants] = useState([]);
+  const [reloadConsultants, setReloadConsultants] = useState(false);
+  const token = getAccessTokenApi();
+
+  useEffect(() => {
+    getConsultants(token, true).then((response) => {
+      setConsultants(response.rows);
+    });
+
+    setReloadConsultants(false);
+  }, [token, reloadConsultants]);
+
   return (
     <div>
-      <h2>mi perfil</h2>
+      <MyProfile
+        consultants={consultants}
+        setReloadConsultants={setReloadConsultants}
+      ></MyProfile>
     </div>
   );
 }
