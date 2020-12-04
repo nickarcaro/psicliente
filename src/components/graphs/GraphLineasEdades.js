0b3681ee684dd0cc32ad026b883a728c2eae7164
Juan Jose Vergara <40677444+JJVergaraManzi@users.noreply.github.com>
2020-12-04 03:31:36 -0300

    Actualizacion de graficos

--- src/components/graphs/GraphLineasEdades.js
+++ src/components/graphs/GraphLineasEdades.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
+import { List, Button } from "antd";
 import { Bar } from "react-chartjs-2";
 
-export default function GraphLinea(props) {
+export default function GraphLineasEdades(props) {
   const { consultants, setReloadConsultants } = props;
   //console.log(consultants);
-  let yAxis = [];
-  let xAxis = [];
+  let edades = [];
+  let motivos = [];
   for (let dataObj of consultants) {
     console.log(dataObj);
-    yAxis.push(dataObj.edad);
-    xAxis.push(dataObj.motivo);
+    edades.push(dataObj.edad);
+    motivos.push(dataObj.motivo);
   }
+
   let data = {
-    labels: xAxis,
+    labels: [0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100],
     datasets: [
       {
         label: "Porcentaje de trastornos mentales",
-        data: yAxis,
-        backgroundColor: ["green", "red", "rgb(255, 153, 51)", "blue"],
+        data: edades,
+        backgroundColor: "rgba(255, 0, 255, 0.75)",
       },
     ],
   };
