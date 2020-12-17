1437b66bcacbdaf116a2446cd0c2f9f99eced296
Juan Jose Vergara <40677444+JJVergaraManzi@users.noreply.github.com>
2020-12-16 23:55:07 -0300

    GraphDoughnut update

--- src/components/Admin/Graphs/GraphDoughnut.js
+++ src/components/Admin/Graphs/GraphDoughnut.js
@@ -10,24 +10,23 @@ export default function GraphDoughnut(props) {
   let ed_PreEsc = [];
   let org_Priv = [];
   for (var dataObj in consultants) {
-    let nameorg = dataObj.tipo_institucion
-    if (nameorg.toLowerCase() === "organización publica") {
+    if (dataObj.tipo_institucion === "organización publica") {
       org_Publica.push(dataObj.tipo_institucion);
       console.log(org_Publica)
     }
-    else if (nameorg.toLowerCase() === "universidad" || nameorg.toLowerCase() === "instituto técnico") {
+    else if (dataObj.tipo_institucion === "universidad" || dataObj.tipo_institucion === "instituto técnico") {
       ed_Sup.push(dataObj.tipo_institucion);
       console.log(ed_Sup)
     }
-    else if (nameorg.toLowerCase() === "colegio" || nameorg.toLowerCase() === "liceo") {
+    else if (dataObj.tipo_institucion === "colegio" || dataObj.tipo_institucion === "liceo") {
       ed_Esc.push(dataObj.tipo_institucion);
       console.log(ed_Esc)
     }
-    else if (nameorg.toLowerCase() === "jardin infantil") {
+    else if (dataObj.tipo_institucion === "jardin infantil") {
       ed_PreEsc.push(dataObj.tipo_institucion);
       console.log(ed_PreEsc)
     }
-    else if (nameorg.toLowerCase() === "organización privada") {
+    else if (dataObj.tipo_institucion === "organización privada") {
       org_Priv.push(dataObj.tipo_institucion);
       console.log(org_Priv)
     }
@@ -42,7 +41,7 @@ export default function GraphDoughnut(props) {
       },
     ]
   };
-
+  console.log(data)
   return <Doughnut
   options={{
     scales: {
