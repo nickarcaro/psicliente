5dc6823a9a116815c0d74679c8652ffbddf4853b
Juan Jose Vergara <40677444+JJVergaraManzi@users.noreply.github.com>
2020-12-18 02:35:48 -0300

    Update GraphLineasEdades.js

--- src/components/Admin/Graphs/GraphLineasEdades.js
+++ src/components/Admin/Graphs/GraphLineasEdades.js
@@ -5,22 +5,31 @@ import { Line } from "react-chartjs-2";
 export default function GraphLineasEdades(props) {
   const { consultants, setReloadConsultants } = props;
   console.log(consultants);
-  let edades = [];
-  let motivos = [];
+  let pacActivo = [];
+  let pacAlAgua = [];
   for (var dataObj in consultants) {
-    console.log(dataObj);
-    edades.push(dataObj.edad);
-    motivos.push(dataObj.motivo);
+    if (dataObj.estado === "consultante" || dataObj.estado === "paciente" ){
+      pacActivo.push(dataObj.edad);
+      console.log(dataObj);
+    }else if (dataObj.estado === "incontestado"){
+      pacAlAgua.push(dataObj.edad);
+      console.log(dataObj);
+    }
   }
 
   let data = {
     labels: [0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100],
     datasets: [
       {
-        label: "Tendencia de consultantes por paciente",
-        data: edades,
-        backgroundColor: "rgba(255, 0, 255, 0.75)",
+        label: "Pacientes activo",
+        data: pacActivo.length,
+        backgroundColor: "#fca311",
       },
+      {
+        label: "Pacientes al agua",
+        data: pacAlAgua.length,
+        backgroundColor: "#14213d",
+      }
     ],
   };
 
