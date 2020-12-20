45657e5d640456a170f9195fbb9783a3284c9020
Juan Jose Vergara <40677444+JJVergaraManzi@users.noreply.github.com>
2020-12-20 01:34:44 -0300

    Update Graphs.js

--- src/components/Admin/Profile/Graphs/Graphs.js
+++ src/components/Admin/Profile/Graphs/Graphs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Bar, Doughnut, Pie } from "react-chartjs-2";
+import { Bar, Doughnut, Pie, Line } from "react-chartjs-2";
 
 export function Graphbar(props) {
   const { consultants, setReloadConsultants } = props;
@@ -63,27 +63,27 @@ export function Graphbar(props) {
   }
 
   let data1 = {
-    labels: [ "Masculino activo", 
-              "Masculino paciente", 
-              "Masculino al agua", 
-              "Mujer activa", 
-              "Mujer paciente", 
-              "Mujer al agua", 
-              "No binario activ-", 
-              "No binario paciente", 
+    labels: [ "Masculino activo",
+              "Masculino paciente",
+              "Masculino al agua",
+              "Mujer activa",
+              "Mujer paciente",
+              "Mujer al agua",
+              "No binario activ-",
+              "No binario paciente",
               "No binario activ-"],
     datasets: [
       {
         label:"Paciente por genero y estado",
-        data:[ masculinoAct.length, 
-               masculinopaciente.length, 
-               masculinoAlagua.length, 
-               femeninoAct.length, 
-               femeninopaciente.length, 
-               femeninoAlagua.length, 
-               femeninoAlagua.length, 
+        data:[ masculinoAct.length,
+               masculinopaciente.length,
+               masculinoAlagua.length,
+               femeninoAct.length,
+               femeninopaciente.length,
+               femeninoAlagua.length,
+               femeninoAlagua.length,
                noBinarioAct.length,
-               noBinarioAlagua.length,  
+               noBinarioAlagua.length,
                 noBinariopaciente.length],
         backgroundColor: "#f77f00",
         borderColor:"#d62828"
@@ -211,7 +211,7 @@ let data2 = {
              adultomayor.length],
       backgroundColor: ["#fb5607",
                         "#4a4e69",
-                        "#f94144", 
+                        "#f94144",
                         "#e500a4"],
     },
   ],
@@ -228,24 +228,6 @@ return <Pie
     }
   }}
   data={data2} />;
-}
-
-  return (
-    <Pie
-      options={{
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      }}
-      data={data2}
-    />
-  );
 }
 
 export function GraphDonut(props) {
@@ -312,4 +294,140 @@ export function GraphDonut(props) {
     }
   }}
    data={data3} />;
+}
+
+export function GraphLineasEstadoPacientes(props) {
+  const { patients, setReloadPatients,  } = props;
+  console.log(patients);
+  //Paciente activo por mes
+  let pacActivo1 = [];
+  let pacActivo2 = [];
+  let pacActivo3 = [];
+  let pacActivo4 = [];
+  let pacActivo5 = [];
+  let pacActivo6 = [];
+  let pacActivo7 = [];
+  let pacActivo8 = [];
+  let pacActivo9 = [];
+  let pacActivo10 = [];
+  let pacActivo11 = [];
+  let pacActivo12
+  //Paciente al agua por mes
+  let pacAlAgua1 = [];
+  let pacAlAgua2 = [];
+  let pacAlAgua3 = [];
+  let pacAlAgua4 = [];
+  let pacAlAgua5 = [];
+  let pacAlAgua6 = [];
+  let pacAlAgua7 = [];
+  let pacAlAgua8 = [];
+  let pacAlAgua9 = [];
+  let pacAlAgua10 = [];
+  let pacAlAgua11 = [];
+  let pacAlAgua12 = [];
+  //sé que este for esta como el reverendo ano de mal optimizado, pero le estoy rezando al pulento para que funcione
+  for (var dataObj in patients) {
+    if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/1/") != -1){
+      pacActivo1.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/2/") != -1){
+      pacActivo2.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/3/") != -1){
+      pacActivo3.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/4/") != -1){
+      pacActivo4.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/5/") != -1){
+      pacActivo5.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/6/") != -1){
+      pacActivo6.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/7/") != -1){
+      pacActivo7.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/8/") != -1){
+      pacActivo8.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/9/") != -1){
+      pacActivo9.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/10/") != -1){
+      pacActivo10.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/11/") != -1){
+      pacActivo11.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "consultante" || dataObj.Estado_id_Estado === "paciente" && dataObj.fecha_ingreso.search("/12/") != -1){
+      pacActivo12.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/1/") != -1){
+      pacAlAgua1.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/2/") != -1){
+      pacAlAgua2.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/3/") != -1){
+      pacAlAgua3.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/4/") != -1){
+      pacAlAgua4.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/5/") != -1){
+      pacAlAgua5.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/6/") != -1){
+      pacAlAgua6.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/7/") != -1){
+      pacAlAgua7.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/8/") != -1){
+      pacAlAgua8.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/9/") != -1){
+      pacAlAgua9.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/1/") != -1){
+      pacAlAgua10.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/1/") != -1){
+      pacAlAgua11.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }else if (dataObj.Estado_id_Estado === "incontestado" && dataObj.fecha_ingreso.search("/1/") != -1){
+      pacAlAgua12.push(dataObj.Estado_id_Estado);
+      console.log(dataObj);
+    }
+  }
+
+  let data5 = {
+    labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+    datasets: [
+      {
+        label: "Pacientes activo",
+        data: [pacActivo1.length,pacActivo2.length,pacActivo3.length,pacActivo4.length,pacActivo5.length,pacActivo6.length,pacActivo7.length,pacActivo8.length,pacActivo9.length,pacActivo10.length,pacActivo11.length,pacActivo12.length],
+        backgroundColor: "#fca311",
+      },
+      {
+        label: "Pacientes al agua",
+        data: [pacAlAgua1.length,pacAlAgua2.length,pacAlAgua3.length,pacAlAgua4.length,pacAlAgua5.length,pacAlAgua6.length,pacAlAgua7.length,pacAlAgua8.length,pacAlAgua9.length,pacAlAgua10.length,pacAlAgua11.length,pacAlAgua12.length],
+        backgroundColor: "#14213d",
+      }
+    ],
+  };
+
+  return <Line
+  options={{
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+           stepSize: 5
+        }
+      }]
+    }
+  }}
+  data={data5} />;
 }
