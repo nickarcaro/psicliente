09d889c299f13cbf81d2c0f32f67fd3bee9f2ad5
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-12-21 20:50:39 -0300

    fix cambio anterior

--- src/pages/Admin/Patients/Patients.js
+++ src/pages/Admin/Patients/Patients.js
@@ -1,34 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { getAccessTokenApi } from "../../../api/auth";
-import { getPatients, getIncontestPatients } from "../../../api/pacientes";
+import { getPatients } from "../../../api/pacientes";
 import ListPatients from "../../../components/Admin/Patients/ListPatients";
 import "./Patients.scss";
 export default function Patients() {
   const [patients, setPatients] = useState([]);
   const [reloadPatients, setReloadPatients] = useState(false);
-  const [reloadInPatients, setReloadInPatients] = useState(false);
-  const [inpatients, setInpatients] = useState([]);
+
   const token = getAccessTokenApi();
 
   useEffect(() => {
     getPatients(token).then((response) => {
       setPatients(response.rows);
     });
-    getIncontestPatients(token).then((response) => {
-      setInpatients(response.rows);
-    });
 
     setReloadPatients(false);
-    setReloadInPatients(false);
-  }, [token, reloadPatients, reloadInPatients]);
+  }, [token, reloadPatients]);
   return (
     <div className="patients">
-      <ListPatients
-        patients={patients}
-        inpatients={inpatients}
-        setReloadInPatients={setReloadInPatients}
-        setReloadPatients={setReloadPatients}
-      />
+      <ListPatients patients={patients} setReloadPatients={setReloadPatients} />
     </div>
   );
 }
